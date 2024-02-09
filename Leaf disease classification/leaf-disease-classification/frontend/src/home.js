32d90fd805acff87ce5661bb659a277df8a0f298
justinvgeorge2024 <justinvgeorge2024@cs.ajce.in>
2024-02-09 12:35:57 +0530

    changes to frontend folder

--- Leaf disease classification/leaf-disease-classification/frontend/src/home.js
+++ Leaf disease classification/leaf-disease-classification/frontend/src/home.js
@@ -9,7 +9,7 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { Paper, CardActionArea, CardMedia, Grid, TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Button, CircularProgress } from "@material-ui/core";
-import cblogo from "./cblogo.PNG";
+// import cblogo from "./cblogo.PNG";
 import image from "./bg.png";
 import { DropzoneArea } from 'material-ui-dropzone';
 import { common } from '@material-ui/core/colors';
@@ -157,10 +157,10 @@ export const ImageUpload = () => {
       let formData = new FormData();
       formData.append("file", selectedFile);
       console.log(formData);
-      
+
       try {
         let res = await axios.post(process.env.REACT_APP_API_URL, formData);
-        
+
         if (res.status === 200) {
           setData(res.data);
         }
@@ -171,7 +171,7 @@ export const ImageUpload = () => {
       }
     }
   };
-  
+
 
   const clearData = () => {
     setData(null);
@@ -212,13 +212,14 @@ export const ImageUpload = () => {
   if (data) {
     confidence = (parseFloat(data.confidence) * 100).toFixed(2);
   }
-
-  function CheckNaN(conf) { if(isNaN(conf)){
-                              return "-"}
-                            else{
-                              return conf
-                              }
+  
+  if (data) {
+    confidence = (parseFloat(data.confidence) * 100).toFixed(2);
   }
+
+
+  function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
+  function confidenceOutput(conf) { if(isNumber(conf)) { return conf; } else { return "-" } }
 
 
   return (
@@ -226,10 +227,10 @@ export const ImageUpload = () => {
       <AppBar position="static" className={classes.appbar}>
         <Toolbar>
           <Typography className={classes.title} variant="h6" noWrap>
-             Leaf Disease Classification
+            Leaf Disease Classification
           </Typography>
           <div className={classes.grow} />
-          <Avatar src={cblogo}></Avatar>
+          {/* <Avatar src={cblogo}></Avatar> */}
         </Toolbar>
       </AppBar>
       <Container maxWidth={false} className={classes.mainContainer} disableGutters={true}>
@@ -273,8 +274,7 @@ export const ImageUpload = () => {
                         <TableCell component="th" scope="row" className={classes.tableCell}>
                           {data.class}
                         </TableCell>
-                        
-                        <TableCell align="right" className={classes.tableCell}>{CheckNaN(confidence)}%</TableCell>
+                        <TableCell align="right" className={classes.tableCell}>{confidenceOutput(confidence)}%</TableCell>
                       </TableRow>
                     </TableBody>
                   </Table>
